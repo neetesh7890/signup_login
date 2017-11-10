2c880a295bac21cab16fe70f294c922a1219b9b0
dev08 <gitk>
2017-11-10 14:53:22 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Form extends React.Component {
+  render() {
+    return (
+      <div className="col-md-12 col-sm-offset-3">
+        <h1>hello</h1>
+      </div>
+
+    );
+  }
+}
+
+
+ReactDOM.render(<Form />, document.getElementById('root'));
+
+
+
